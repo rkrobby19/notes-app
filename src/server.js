e3492ee7d10cf74d092dd213c79f540acdac861b
rkrobby19 <rizkyrobby19@gmail.com>
2023-02-04 10:09:14 +0700

    Add cors policy

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const init = async () => {
   const server = Hapi.server({
     port: 3000,
     host: "localhost",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
   });
 
   server.route(routes);
