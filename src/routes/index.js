2b8a2c301195d254c9bea5818f56708eed1db0fa
rkrobby19 <rizkyrobby19@gmail.com>
2023-02-04 11:03:42 +0700

    Add CRUD to note handler

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,36 @@
-const { addNoteHandler } = require("../controllers");
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  editNoteByIdHandler,
+  getNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require("../controllers");
 
 const routes = [
   {
     method: "POST",
     path: "/notes",
     handler: addNoteHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
   },
 ];
 
